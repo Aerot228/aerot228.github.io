785caeb6b41a4c7287d6ff66319c873636f3c9cd
Aerot228 <66784642+Aerot228@users.noreply.github.com>
2021-06-17 13:14:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,54 @@
-const classNames = {
-    TODO_ITEM: 'todo-container',
-    TODO_CHECKBOX: 'todo-checkbox',
-    TODO_TEXT: 'todo-text',
-    TODO_DELETE: 'todo-delete',
-}
-
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
-
-let todos = [{ id: 1, text: 'task 1', check: true }, { id: 2, text: 'task 2', check: false }, { id: 3, text: 'task 3', check: true }];
+const Todo = props =>(
+  <li>
+    <input type="checkbox" checked = {props.todo.checked}/>
+    <button onClick = {props.onDelete}>delete</button>
+    <span>{props.todo.text}</span>
+  </li>
+)
 let id = 0;
-if (todos.length != 0) {
-    render();
-}
-class Todo {
-    constructor() {
-        this.text = this.getText();
-        this.check = false;
-        this.id = id++;
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      todos:[],
     }
-    getText() {
-        return prompt("Enter task to do:");
-    }
+  }
+  toggleTodo(id){
+    this.setState=({
+      todos: this.state.todos.map(todo =>{
+        if(todo.id !== id) return todo
+        return
+        {
+          id: todo.id;
+          text: todo.text;
+          checked: !todo.checked;
+        }
+      })
+    })
+  }
+  addTodo(){
+    const text = prompt("Enter TODO");
+    this.setState=({
+      todos:[...this.state.todos,{id: id++,text: text, checked: false}]
+    })
+  }
+  removeTodo(id){
+    this.setState({
+      todos: this.state.todos.filter(todo => todo !== todo.id)
+    })
+  }
+  render(){
+    return(
+      <div>
+        <div>Todo count:{this.state.todos.length}</div>
+        <div>Unchecked todo count:{this.state.todo.filter(todo=>!todo.checked).length}</div>
+        <button onClick = {() =>this.addTodo}>add Todo</button>
+        <ul>{this.state.todos.map(todo => <Todo 
+        onToggle={()=>this.toggleTodo(todo.id)}
+        onDelete={()=>this.removeTodo(todo.id)} 
+        todo = {todo}/>)}</ul>
+      </div>
+    )
+  }
 }
-function newTodo() {
-
-    const todo = new Todo();
-    todos.push(todo);
-    render();
-}
-
-function render() {
-    list.innerHTML = '';
-    todos.map(renderTodo).forEach(todo => list.appendChild(todo));
-    todos.map(renderTodo).forEach(todo => localStorage.setItem(todo,todo.text));
-    itemCountSpan.textContent = todos.length;
-    uncheckedCountSpan.textContent = todos.filter(todo => !todo.check).length;
-}
-
-function deleteTodo(id) {
-    todos = todos.filter(todo => todo.id !== id);
-    render();
-}
-
-function renderTodo(todo) {
-    const li = document.createElement('li');
-    li.innerHTML = ` 
-    <input type="checkbox" onchange="changeTodo(${todo.id}) " ${todo.check ? "checked" : ""}>
-    <button onclick="deleteTodo(${todo.id})"> delete </button>
-    <span class="todo-text">${todo.text} </span>
-    `
-    return li;
-}
-
-function changeTodo(id) {
-    todos = todos.map(todo => todo.id == id ? {...todo, check: !todo.check } : todo);
-    uncheckedCountSpan.textContent = todos.filter(todo => !todo.check).length;
-}
+ReactDOM.render(<App/>, document.getElementById('root'));
